35a27518a5bf52b8a1553cfd7056095bbd7f8a9f
owenmeyer3 <owenmeyer3@gmail.com>
2021-01-23 12:05:55 -0600

    removed unnecessary files

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,25 @@
-var rawData = document.currentScript.getAttribute("indata")
-var decodeData = decodeURIComponent(JSON.parse('"'+rawData+'"'));
-console.log(decodeData)
-var data = JSON.parse(JSON.parse('"'+decodeData+'"'))
-console.log(typeof data)
+// var rawData = document.currentScript.getAttribute("indata")
+// var decodeData = decodeURIComponent(JSON.parse('"'+rawData+'"'));
+// console.log(decodeData[0])
+//var data = JSON.parse(JSON.parse('"'+decodeData+'"'))
+//console.log(data[0])
 
-d3.select('#panel1')
-    .selectAll('div')
-    .data(data).enter()
-    .append('div')
-    .html(
-        d => `<table>
-            <tr>
-                <th>Id</th>
-                <th>${d['id']}</th>
-            </tr>
-            <tr>
-                <td>Title</td>
-                <td>${d['title']}</td>
-            </tr>
-            <tr>
-                <td>Company</td>
-                <td>${d['company']}</td>
-            </tr>  
-        </table>`)
+// d3.select('#panel1')
+//     .selectAll('div')
+//     .data(data).enter()
+//     .append('div')
+//     .html(
+//         d => `<table>
+//             <tr>
+//                 <th>Id</th>
+//                 <th>${d['id']}</th>
+//             </tr>
+//             <tr>
+//                 <td>Title</td>
+//                 <td>${d['title']}</td>
+//             </tr>
+//             <tr>
+//                 <td>Company</td>
+//                 <td>${d['company']}</td>
+//             </tr>  
+//         </table>`)
