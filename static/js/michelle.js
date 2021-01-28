870441f894abc7bcc506577681398c84cf020987
michellebannon <70609523+michellebannon@users.noreply.github.com>
2021-01-28 16:28:44 -0600

    Started building code to create a table
    I worked with my tutor and we decided to start to build a table because there wasn't salary data that would allow us to make a gauge using salary.  I am going to use the company data to identify how many times a company has an open for the job title that is searched.

--- static/js/michelle.js
+++ static/js/michelle.js
@@ -1,2 +1,23 @@
 // To run, type 'flask run' in the terminal. Ctrl + click on url to open page
 var data = JSON.parse(d3.select('#dataScript').attr('indata'));
+
+var title = d3.select('#jobType').property('value');
+var uniqueCompanies = new Set(data.map(obj => obj.company));
+
+d3.select('#michelle').append('h1').text(title).attr('class','text-center display-3').append('hr');
+var table = d3.select('#michelle').append('table').attr('class','table table-stripped');
+table.append('thead').append('tr').append('th').text('Compay Name').append('th').append('th').text('Date').append('tbody');
+
+data.forEach(obj => {
+    var row = table.append('tr')
+    Object.entries(([key,val])=>{
+        if (key == 'company') {
+            var cell1 = row.append('td');
+            cell1.text(val)
+        };
+        if (key == 'createdAt') {
+            var cell2 = row.append('td');
+            cell2.text(val)
+        };
+    })
+});
