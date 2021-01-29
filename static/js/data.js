8de687652a0416a4e0ac3887a542d95a68fe790c
owenmeyer3 <owenmeyer3@gmail.com>
2021-01-29 13:02:58 -0600

    changed data layout on page

--- static/js/data.js
+++ static/js/data.js
@@ -1,23 +1,27 @@
 // Get data from html <script> object 'indata' param
 var data = JSON.parse(document.currentScript.getAttribute("indata"));
 
+
+var myDiv = d3.select('#owen');
+myDiv.html('<table></table>')
+var myTable = myDiv.select('table')
+myTable.html(`<tr>
+                <th>Title</th>
+                <th>Company</th>
+                <th>Latitide</th>
+                <th>Longitude</th>
+                <th>Location Name</th>
+            </tr>`)
+
 // Make table for each job returned
-d3.select('#owen')
-    .selectAll('div')
+myTable
+    .selectAll('tr')
     .data(data).enter()
-    .append('div')
+    .append('tr')
     .html(d => 
-        `<table>
-            <tr>
-                <th>Id</th>
-                <th>${d['id']}</th>
-            </tr>
-            <tr>
-                <td>Title</td>
-                <td>${d['title']}</td>
-            </tr>
-            <tr>
-                <td>Company</td>
-                <td>${d['company']}</td>
-            </tr>  
-        </table>`)
+        `<td>${d['title']}</td>
+        <td>${d['company']}</td>
+        <td>${d['lat']}</td>
+        <td>${d['lng']}</td>
+        <td>${d['locationName']}</td>`
+    )
