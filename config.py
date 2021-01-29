59fa577bda5596066f3baa8a97354eb2b5e806ad
michellebannon <70609523+michellebannon@users.noreply.github.com>
2021-01-28 19:38:46 -0600

    Added my Adzuna key

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ app_key = '672a581c8ecccf03d3905935ded22d61'
 # Owen's Keys
 # app_id = '2ae8e9f7'
 # app_key = '672a581c8ecccf03d3905935ded22d61'
+
+# Michelles Keys
+# app_id = '45c601a0'
+# app_key = '135eaf59433896f9b62ecb11f539856f'
