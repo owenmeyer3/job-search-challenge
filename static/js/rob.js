e20e3a92fd104e68981841e8acfc5a58c4a0a225
robertjbowen <69124118+robertjbowen@users.noreply.github.com>
2021-01-28 23:32:10 -0600

    updated rob.js to add map functionality and data markers, adjusted map size in style.css

--- static/js/rob.js
+++ static/js/rob.js
@@ -2,35 +2,72 @@
 var data = JSON.parse(d3.select('#dataScript').attr('indata'));
 console.log(data);
 
-// Create the tile layer that will be the background of our map
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "light-v10",
+// Select the tile layer that will be the background of our map
+var satelliteMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  maxZoom: 20,
+  id: 'mapbox/satellite-v9',
   accessToken: API_KEY
 });
 
-// Initialize all of the LayerGroups we'll be using
-var layers = {
-  COMING_SOON: new L.LayerGroup(),
-  EMPTY: new L.LayerGroup(),
-  LOW: new L.LayerGroup(),
-  NORMAL: new L.LayerGroup(),
-  OUT_OF_ORDER: new L.LayerGroup()
-};
-
-// Create the map with our layers
-var map = L.map("rob", {
-  center: [40, -90], //[40.73, -74.0059],
-  zoom: 5,
-  layers: [
-    layers.COMING_SOON,
-    layers.EMPTY,
-    layers.LOW,
-    layers.NORMAL,
-    layers.OUT_OF_ORDER
-  ]
+var streetsMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  maxZoom: 20,
+  id: 'mapbox/streets-v11',
+  accessToken: API_KEY
 });
 
-// Add our 'lightmap' tile layer to the map
-lightmap.addTo(map);
+var outdoorsMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    maxZoom: 20,
+    id: 'mapbox/outdoors-v11',
+    accessToken: API_KEY
+});
+
+var lightMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    maxZoom: 20,
+    id: 'mapbox/light-v10',
+    accessToken: API_KEY
+});
+
+var darkMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    maxZoom: 20,
+    id: 'mapbox/dark-v10',
+    accessToken: API_KEY
+});
+
+var mapStyle = {
+  'Dark': darkMap,
+  'Light': lightMap,
+  'Outdoors': outdoorsMap,  
+  'Satellite': satelliteMap,
+  'Streets': streetsMap
+};
+
+// Initialize the LayerGroups
+var layers = {
+  icons: new L.LayerGroup(),
+  chloropleth: new L.LayerGroup(),
+};
+
+// Create the map with data layers
+var myMap = L.map("rob", {
+  center: [38, -95],
+  zoom: 5,
+  layers: [ lightMap, layers.icons]
+});
+
+var overlays = {
+  'Chloropleth': layers.chloropleth,
+  'Icons': layers.icons
+};
+
+L.control.layers(mapStyle, overlays, {
+  collapsed: false
+}).addTo(myMap);
+
+// Loop through data
+for (var i = 0; i < data.length; i++) {  
+  
+  // Create a new marker at the appropriate coordinates and bind a popup to it
+  var newMarker = L.marker([data[i].lat, data[i].lng])
+    .bindPopup("Job Title: " + data[i].title + "<br> Company: " + data[i].company + "<br> Date Posted: "+ new Date(data[i].createdAt) + "<br> Category: "+ data[i].category)
+    .addTo(layers.icons);
+}; 
