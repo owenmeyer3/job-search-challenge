52b7c98a8a5f08447eb563a6d8c73b72bf63347d
robertjbowen <69124118+robertjbowen@users.noreply.github.com>
2021-01-28 20:13:12 -0600

    updated rob.js index.html to add leaflet functionality

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoicmpib3dlbiIsImEiOiJja2p4azltNmgwNHYyMnZxdzk3YjdhcmxpIn0.dSmZbFh51oc10HQmgeP_dA";
--- static/js/rob.js
+++ static/js/rob.js
@@ -1,2 +1,36 @@
 // To run, type 'flask run' in the terminal. Ctrl + click on url to open page
 var data = JSON.parse(d3.select('#dataScript').attr('indata'));
+console.log(data);
+
+// Create the tile layer that will be the background of our map
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
+// Initialize all of the LayerGroups we'll be using
+var layers = {
+  COMING_SOON: new L.LayerGroup(),
+  EMPTY: new L.LayerGroup(),
+  LOW: new L.LayerGroup(),
+  NORMAL: new L.LayerGroup(),
+  OUT_OF_ORDER: new L.LayerGroup()
+};
+
+// Create the map with our layers
+var map = L.map("rob", {
+  center: [40, -90], //[40.73, -74.0059],
+  zoom: 5,
+  layers: [
+    layers.COMING_SOON,
+    layers.EMPTY,
+    layers.LOW,
+    layers.NORMAL,
+    layers.OUT_OF_ORDER
+  ]
+});
+
+// Add our 'lightmap' tile layer to the map
+lightmap.addTo(map);
