a6f03a3094fed0b8902fa22609e317b7e0cdd489
michellebannon <70609523+michellebannon@users.noreply.github.com>
2021-01-29 17:01:39 -0600

    Bar chart using plotly
    After working with my tutor results are published

--- static/js/michelle.js
+++ static/js/michelle.js
@@ -1,21 +1,5 @@
 var data = JSON.parse(d3.select('#dataScript').attr('indata'));
 console.log(data)
-// this is the code I created independently that uses another html file
-// how do I use data from our source and get the same results?
-
-const data1 = [
-    {company: 'Epsilon', openings: 19 }, //'Epsilon
-    {company: 'IQVIA', openings: 9 }, //'IQVIA
-    {company: 'Epsilon', openings: 7 },
-    {company: 'KPMG', openings: 7 },
-    {company: 'PwC', openings: 6 },
-    {company: 'Fetch Rewards', openings: 6 },
-    {company: 'Epsilon', openings: 5 },
-    {company: 'physIQ', openings: 5 },
-    {company: 'IBM', openings: 5 },
-    {company: 'Robert Half', openings: 5 },
-    {company: 'The University of Chicago', openings: 4 }
-];
 
 // gets all the companies for each job
 var companies = (data.map(obj => obj.company));
@@ -34,55 +18,29 @@ uniqueCompanies.forEach(uniqueCompany=>{
     })
     companyCounts.push({'company':uniqueCompany, 'count':count})
 })
+
 var sortedCompanyCounts = companyCounts.sort((a, b) => d3.descending(a.count, b.count));
-var topCompanyInt = 10
-var topSortedCompanyCounts = sortedCompanyCounts.slice(0,topCompanyInt)
-console.log(topSortedCompanyCounts)
+var topSortedCompanyCounts = sortedCompanyCounts.slice(0,10);
 
-const width = 1600;
-const height = 400;
-const margin = {top: 50, bottom: 50, left: 50, right: 50 };
-
-const svg = d3.select('#michelle')
-    .append('svg')
-    .attr('height', height - margin.top - margin.bottom)
-    .attr('width', width - margin.left - margin.right)
-    //.attr('viewBox', [0, 0, width, height]);
-
-const x = d3.scaleBand()
-    .domain(d3.range(topSortedCompanyCounts.length))
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
-
-
-const y = d3.scaleLinear()
-    .domain([0, 25])
-    .range([height - margin.bottom, margin.top]);
-   
-svg
-    .append('g')
-    .attr('fill', 'purple')
-    .selectAll('rect')
-    .data(topSortedCompanyCounts)
-    .join('rect')
-        .attr('x', (d) => x(d.company))
-        .attr('y', (d) => y(d.count))
-        .attr('height', d => y(0) - y(d.count))
-        .attr('width', x.bandwidth())
-        .attr('class', 'rectangle')
-
-function xAxis(g) {
-    g.attr('transform', `translate(0, ${height - margin.bottom})`)
-    //.call(d3.axisBottom(x).tickFormat(i => data[i].company))
-    //.attr('font-size', '14px')
-
-}
-function yAxis(g) {
-    g.attr('transform', `translate(${margin.left}, 0)`)
-        // .call(d3.axisLeft(y).ticks(null, data.format))
-        // .attr('font-size', '14px')
-}
-
-svg.append('g').call(yAxis);
-svg.append('g').call(xAxis);
-// svg.node();
+  var data = [{
+    x: topSortedCompanyCounts.map(obj => obj.company),
+    y: topSortedCompanyCounts.map(obj => obj.count),
+    type: 'bar',
+    text: topSortedCompanyCounts.map(obj => obj.count),
+    textposition: 'auto',
+    hoverinfo: 'none',
+    // opacity: 0.5,
+    marker: {
+      color: '#8f5db0',
+      line: {
+        color: '#8f5db0',
+        width: 1.5
+      }
+    }
+  }];
+  
+  var layout = {
+    title: 'Job Postings by companies'
+  };
+  
+  Plotly.newPlot('michelle', data, layout);
