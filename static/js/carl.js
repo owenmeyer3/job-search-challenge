a42bbd50ee7e4ed87263f7c81400a4f6437daabc
CCC-GH <70607540+CCC-GH@users.noreply.github.com>
2021-01-30 11:30:49 -0600

    Update carl.js

--- static/js/carl.js
+++ static/js/carl.js
@@ -1,7 +1,7 @@
 // To run, type 'flask run' in the terminal. Ctrl + click on url to open page
 var data = JSON.parse(d3.select('#dataScript').attr('indata'));
 
-console.log(data)
-for (i = 0; i < data.length; i++) {
-    print("Title:" + data[i].title + "<br> Company:" + data[i].company + "<br> Posted:" + new Date(data[i].createdAt) + "<br> Category: " + data[i].category)
-}
+//console.log(data)
+//for (i = 0; i < data.length; i++) {
+//    print("Title:" + data[i].title + "<br> Company:" + data[i].company + "<br> Posted:" + new Date(data[i].createdAt) + "<br> Category: " + data[i].category)
+//}
