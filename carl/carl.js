220a66fedaeca3ac6c900ab62fcb7182eb4677d0
CCC-GH <70607540+CCC-GH@users.noreply.github.com>
2021-01-30 09:03:40 -0600

    Test Update

--- carl/carl.js
+++ carl/carl.js
@@ -1,51 +1,47 @@
-// https://api.adzuna.com/v1/api/jobs/us/top_companies?app_id=86767575&app_key=adc9c2040851b81df96181b91cfc65a1&what=Data%20Science&location0=Chicago
+// Top Companies: https://api.adzuna.com/v1/api/jobs/us/top_companies?app_id=86767575&app_key=adc9c2040851b81df96181b91cfc65a1&what=Data%20Science&location0=Chicago
+// Categories:  http://api.adzuna.com/v1/api/jobs/gb/categories?app_id={YOUR API ID}&app_key={YOUR API KEY}&&content-type=application/json
+// Salary: http://api.adzuna.com/v1/api/jobs/gb/history?app_id={YOUR API ID}&app_key={YOUR API KEY}&location0=UK&location1=London&category=it-jobs&content-type=application/json
+// data = JSON.parse(d3.select('#dataScript').attr('indata'));
 
-
-var margin = {
-        top: 20,
+margin = {
+        top: 40,
         right: 20,
         bottom: 70,
         left: 40
     },
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+    width = 800 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
 // set the ranges
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-
-var y = d3.scale.linear().range([height, 0]);
+x = d3.scale.ordinal().rangeRoundBands([0, width], .2);
+y = d3.scale.linear().range([height, 5]);
 
 // define the axis
-var xAxis = d3.svg.axis()
+xAxis = d3.svg.axis()
     .scale(x)
     .orient('bottom')
-
-
-var yAxis = d3.svg.axis()
+yAxis = d3.svg.axis()
     .scale(y)
     .orient('left')
-    .ticks(10);
-
-
+    .ticks(6);
 // add the SVG element
-var svg = d3.select('body').append('svg')
+svg = d3.select('body').append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
     .attr('transform',
-        'translate(' + margin.left + ',' + margin.top + ')');
-
+        `translate(${margin.left},${margin.top})`);
 
 // load the data
 d3.json('data.json', function(error, data) {
 
     data.forEach(function(d) {
-        d.Letter = d.Letter;
+        d.Company = d.Company;
         d.Freq = +d.Freq;
     });
 
     // scale the range of the data
     x.domain(data.map(function(d) {
-        return d.Letter;
+        return d.Company;
     }));
     y.domain([0, d3.max(data, function(d) {
         return d.Freq;
@@ -57,20 +53,18 @@ d3.json('data.json', function(error, data) {
         .attr('transform', 'translate(0,' + height + ')')
         .call(xAxis)
         .selectAll('text')
-        .style('text-anchor', 'end')
-        .attr('dx', '-.8em')
-        .attr('dy', '-.55em')
-        .attr('transform', 'rotate(-45)');
+        .style('text-anchor', 'center')
+        .attr('dx', '0em')
+        .attr('dy', '1em')
+        .attr('transform', 'rotate(0)');
 
     svg.append('g')
         .attr('class', 'y axis')
         .call(yAxis)
         .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 5)
-        .attr('dy', '.71em')
-        .style('text-anchor', 'end')
-        .text('#Open Jobs');
+        .attr('y', -25)
+        .attr('dy', '1em')
+        .text('Top 5 Hiring Companies');
 
     // Add bar chart
     svg.selectAll('bar')
@@ -78,7 +72,7 @@ d3.json('data.json', function(error, data) {
         .enter().append('rect')
         .attr('class', 'bar')
         .attr('x', function(d) {
-            return x(d.Letter);
+            return x(d.Company);
         })
         .attr('width', x.rangeBand())
         .attr('y', function(d) {
