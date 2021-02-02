548e2abc8afbb8cb937295b54bac8882e6ef2d97
CCC-GH <70607540+CCC-GH@users.noreply.github.com>
2021-02-02 01:55:46 -0600

    Feb1 - Mouseover added
    needed d3-tip js library added to index.html and add'll css defined for mouseover (box and pointer to bar)

--- static/js/carl.js
+++ static/js/carl.js
@@ -1,10 +1,10 @@
 data = JSON.parse(d3.select('#dataScript').attr('indata'));
 
-var chartTitle = 'Top 5 Job Titles';
-var topN = 5;
+// set default top-N select
+chartTitle = 'Top 5 Job Titles';
+topN = 5;
 generateChart();
 
-// top-N select
 function onSelect() {
     topN = document.getElementById('topN').value;
     chartTitle = 'Top ' + topN + ' Job Titles';
@@ -17,7 +17,7 @@ function generateChart() {
             top: 40,
             right: 20,
             bottom: 70,
-            left: 40
+            left: 45
         },
         width = 600 - margin.left - margin.right,
         height = 300 - margin.top - margin.bottom;
@@ -34,6 +34,7 @@ function generateChart() {
         .scale(y)
         .orient('left')
         .ticks(6);
+
     // add SVG element
     svg = d3.select('#chart').append('svg')
         .attr('width', width + margin.left + margin.right)
@@ -42,32 +43,37 @@ function generateChart() {
         .attr('transform',
             `translate(${margin.left},${margin.top})`);
 
-    // for stored json data
-    // d3.json('static/js/SampleData_Default.json', function(error, data) {
+    // for stored json data, un-rem line below and 2nd from bottm->"})"
+    //d3.json('../static/js/SampleData_Default.json', function(error, data) {
+
+    // pulling titles and stripping bold/strong tags
+    titles = (data.map(obj => obj.title.replace('<strong>', '').replace('</strong>', '')));
 
     // identify unique titles
-    titles = (data.map(obj => obj.title.replace('<strong>', '')));
     uniqueTitles = [...new Set(titles)];
-    titlelist = [];
 
+    // create an array of json objects - "pick" will select the "title" attribute from raw json (data)
+    //   for "_.pick()"" function add Lodash library: 
+    //      (index.html) src='https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js'
+    titleList = [];
     data.forEach(function(d) {
-        titlelist.push(_.pick(d, ['title']));
+        titleList.push(_.pick(d, ['title']));
     });
 
-    // identify counts of unique titles
+    // loop-count unique titles and push both to new array of json-objects, titleCounts
     titleCounts = []
     uniqueTitles.forEach(function(uniq, index) {
         count = 0
-        titlelist.forEach(t => {
-            if (uniq === t.title) {
+        titleList.forEach(t => {
+            if (uniq === t.title.replace('<strong>', '').replace('</strong>', '')) {
                 count += 1
             }
         })
-        titleCounts.push({ 'title': uniq, 'Freq': count })
+        titleCounts.push({ 'title': uniq, 'freq': count })
     });
 
-    // sort and select chosen Top-N
-    titleCounts = titleCounts.sort((a, b) => d3.descending(a.Freq, b.Freq));
+    // sort dictionary and user-select-chosen Top-N
+    titleCounts = titleCounts.sort((a, b) => d3.descending(a.freq, b.freq));
     titleCounts = titleCounts.slice(0, topN);
 
     // scale range
@@ -75,10 +81,10 @@ function generateChart() {
         return d.title;
     }));
     y.domain([0, d3.max(titleCounts, function(d) {
-        return d.Freq;
+        return d.freq;
     })]);
 
-    // set axis
+    // set x-axis
     svg.append('g')
         .attr('class', 'x axis')
         .attr('transform', 'translate(0,' + height + ')')
@@ -90,6 +96,7 @@ function generateChart() {
         .attr('dy', '1em')
         .attr('transform', 'rotate(12)');
 
+    // set y-axis
     svg.append('g')
         .attr('class', 'y axis')
         .call(yAxis)
@@ -99,22 +106,34 @@ function generateChart() {
         .attr('dy', '1em')
         .text(chartTitle)
         .style('font-size', '15pt');
-
-    // Add bar chart
+    // mouseover
+    tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .style('font-size', '10pt')
+        .html(function(d) {
+            return "<span style='color:white'>" + d.title +
+                "</span><br/>count: <span style='color:white'>" + d.freq + "</span>";
+        })
+    svg.call(tip);
+    // bar chart
     svg.selectAll('bar')
         .data(titleCounts)
         .enter().append('rect')
+        .style('width', function(d) { return x(d) + 'px'; })
+        .text(function(d) { return d; })
         .attr('class', 'bar')
         .attr('x', function(d) {
             return x(d.title);
         })
         .attr('width', x.rangeBand())
         .attr('y', function(d) {
-            return y(d.Freq);
+            return y(d.freq);
         })
         .attr('height', function(d) {
-            return height - y(d.Freq);
+            return height - y(d.freq);
         })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide)
         //});
-
 }
